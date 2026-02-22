909cc88468b25885d92c185942fcf51a05adaa7e
tha-is <thscostav@gmail.com>
2026-02-22 13:57:32 -0300

    corrigindo caminho da imagem do HEADER

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import imagemHeader from '../assets/Icone.png'
 
 const Header = () => {
     const headerInfo = {
@@ -10,7 +11,7 @@ const Header = () => {
         <header className='row row-cols-1 row-cols-md-2'>
             <div className='col'>
                 <img 
-                    src="./src/assets/Icone.png" 
+                    src={imagemHeader} 
                     alt="Logo" 
                     className="img-fluid object-fit-cover"/>
             </div>
